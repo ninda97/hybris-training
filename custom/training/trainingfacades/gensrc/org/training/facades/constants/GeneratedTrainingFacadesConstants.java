b75194cf286ca90dccc233fc743e7fb2ba8a23f5
Ninda <ninda.rib97@gmail.com>
2022-01-24 09:09:37 +0700

    add custom and config

---
+++ custom/training/trainingfacades/gensrc/org/training/facades/constants/GeneratedTrainingFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jan 21, 2022, 3:14:56 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.training.facades.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingFacadesConstants
+{
+	public static final String EXTENSIONNAME = "trainingfacades";
+	
+	protected GeneratedTrainingFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ custom/training/traininginitialdata/gensrc/org/training/initialdata/constants/GeneratedTrainingInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jan 21, 2022, 3:14:56 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.training.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "traininginitialdata";
+	
+	protected GeneratedTrainingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
